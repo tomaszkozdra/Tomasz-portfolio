8353f0738ac1d7b651bcd17d71e47f3f162fb557
Tomasz Kozdra <tkozdra2008@gmail.com>
2025-03-28 15:09:53 +0100

    Twoje zmiany

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter basename="/tomasz-portfolio">
+  <BrowserRouter basename="/Tomasz-portfolio">
     <App />
   </BrowserRouter>
 );
